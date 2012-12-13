66f7db067c786421061a36add16b5cdcd27ef022
Mihail Velikov <mihailvelik@gmail.com>
2012-12-13 10:41:11 +0200

    rempved debug

--- public/index.php
+++ public/index.php
@@ -21,6 +21,6 @@ require_once 'Zend/Application.php';
 $application = new Zend_Application(
     APPLICATION_ENV,
     APPLICATION_PATH . '/configs/application.ini'
-);
+);echo '<pre>', var_dump($application), '</pre>';exit();
 $application->bootstrap()
             ->run();
